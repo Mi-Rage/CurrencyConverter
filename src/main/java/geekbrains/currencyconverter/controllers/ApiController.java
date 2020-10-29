c2675e69c0dd33edc27527927f0e24114396341f
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2020-10-29 14:33:34 +0300

    Реализация расчета итоговой суммы
    Реализован код с запросом на главной странице пары валюты и количества валюты для конвертации. А так же вывод на странице результата всех введенных данных и рассчитанной итоговой суммы.

--- src/main/java/geekbrains/currencyconverter/controllers/ApiController.java
+++ src/main/java/geekbrains/currencyconverter/controllers/ApiController.java
@@ -25,8 +25,9 @@ public class ApiController {
 
     /**
      * По нажатию кнопки "Submit" на странице запроса имени пары будет вызван этот метод
-     * Здесь мы получаем имя искомой пары, получаем её цену и сохраняем в репозитории на всякий
+     * Здесь мы получаем имя искомой пары, получаем её цену и к-во к конвертации, сохраняем в репозитории на всякий
      * Зачем сохраняем? Потому что можем.
+     * Производим расчет итоговой суммы
      * @param newPairs - объект из формы на странице запроса пары
      * @return String, данные для проверки в POSTMAN или можно использовать как свой API
      * @throws IOException - вылетит если нет такой пары
@@ -34,12 +35,25 @@ public class ApiController {
     @PostMapping(path = "/api/new", produces = "application/json;charset=UTF-8")
     public String setApiCode(@RequestBody Pairs newPairs) throws IOException {
         Pairs responsePairs = new Pairs();
+
         String pairs = newPairs.getPairName();
         responsePairs.setPairName(pairs);
-        responsePairs.setPairPrice(Parser.getLastPriseByPairs(pairs));
+
+        String quantity = newPairs.getQuantity();
+        responsePairs.setQuantity(quantity);
+
+        String lastPairPrice = Parser.getLastPriseByPairs(pairs);
+        responsePairs.setPairPrice(lastPairPrice);
+
+        float amount = Integer.parseInt(quantity) * Float.parseFloat(lastPairPrice);
+        responsePairs.setAmount(String.valueOf(amount));
+
         service.setToRepository(responsePairs);
+
         String response = "{ \"id\" : \"" + responsePairs.getPairName() + "\" , \n" +
-                " \"price\" : \"" + responsePairs.getPairPrice() + "\" } \n";
+                " \"price\" : \"" + responsePairs.getPairPrice() + "\" , \n" +
+                " \"quantity\" : \"" + responsePairs.getQuantity() + "\", \n" +
+                " \"amount\" : \"" + responsePairs.getAmount() + "\" } \n";
         System.out.println(response);
         return response;
     }
--- src/main/java/geekbrains/currencyconverter/model/Pairs.java
+++ src/main/java/geekbrains/currencyconverter/model/Pairs.java
@@ -4,6 +4,8 @@ public class Pairs {
 
     private String pairName;
     private String pairPrice;
+    private String quantity = "0";
+    private String amount = "0";
 
     public Pairs() {
     }
@@ -22,6 +24,22 @@ public class Pairs {
 
     public void setPairPrice(String pairPrice) {
         this.pairPrice = pairPrice;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
     }
 }
 
